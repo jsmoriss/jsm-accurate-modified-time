5884ef57102470c848373084c917319310cf5552
jsmoriss <jsm@surniaulula.com>
2024-09-10 18:25:38 +0000

    v2.0.0

--- jsm-accurate-modified-time.php
+++ jsm-accurate-modified-time.php
@@ -12,7 +12,7 @@
  * Description: Update post/page modified times when output from post/page shortcodes and blocks changes.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.6.1
+ * Tested Up To: 6.6.2
  * Version: 2.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
