f39c129178017a8f6f440a21ed30d664cbd0b033
jsmoriss <jsm@surniaulula.com>
2024-09-14 22:08:46 +0000

    v2.0.0

--- jsm-accurate-modified-time.php
+++ jsm-accurate-modified-time.php
@@ -10,8 +10,8 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Update post/page modified times when output from post/page shortcodes and blocks changes.
- * Requires PHP: 7.2.34
- * Requires At Least: 5.8
+ * Requires PHP: 7.4.33
+ * Requires At Least: 5.9
  * Tested Up To: 6.6.2
  * Version: 2.0.0
  *
